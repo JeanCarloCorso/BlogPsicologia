60b481d8597e3b0c52faafd1b9be713e2b796521
Jean Carlo Corso <jeancarlocorso@hotmail.com>
2024-02-06 15:56:14 -0300

    bloco contato concluido

--- BlogPsicologia/resources/views/home.blade.php
+++ BlogPsicologia/resources/views/home.blade.php
@@ -130,5 +130,34 @@
     </div>
 </div>
 
+<div class="row g-3 custon-row-sobre justify-content-center" style="margin-bottom: 50px;">
+    <form class="col-sm-10 col-md-8 col-lg-6" method="post" >
+        @csrf
+        <div class="custon-bloco">
+            <h3>CONTATO</h3><br>
+        </div>
+
+        <div class="form-floating mb-3">
+            <input type="text" name="nome" id="txtNomeCompleto" class="form-control" placeholder=" " autofocus>
+            <label for="txtNomeCompleto">Nome Completo</label>
+        </div>
+
+        <div class="form-floating mb-3">
+            <input type="email" id="txtEmail" name="email" class="form-control" placeholder=" ">
+            <label for="txtEmail">E-mail</label>
+        </div>
+
+        <div class="form-floating mb-3">
+            <textarea id="txtMensagem" name="mensagem" class="form-control" placeholder=" "
+                style="height: 200px;"></textarea>
+            <label for="txtMensagem">Mensagem</label>
+        </div>
+
+        <div class="col-12">
+            <button type="submit" class="btn btn-lg btn-custon w-100">Enviar Mensagem</button>
+        </div>
+    </form>
+</div>
+
 @endsection
 
