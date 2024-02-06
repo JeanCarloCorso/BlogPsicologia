4669e17aac5bd24b0a831a016d09f426aa94f3ed
Jean Carlo Corso <jeancarlocorso@hotmail.com>
2024-02-06 12:51:46 -0300

    adicionando cards de artigos recentes | não finalizado

--- BlogPsicologia/resources/views/home.blade.php
+++ BlogPsicologia/resources/views/home.blade.php
@@ -16,4 +16,52 @@
         <div class="bar"></div>
     </div>
 </div>
+
+@endsection
+
+@section('content')
+
+<div class="custon-bloco">
+    <h3>ARTIGOS MAIS RECENTES</h3>
+    <hr class="custon">
+    <div class="row g-3 justify-content-center custon-row">
+        <div class="col-xl-6 col-lg-6 col-md-6 col-6">
+            <div class="row g-3 justify-content-center">
+                <a href="/" class="btn mt-2 d-flex align-items-center">
+                    <div class="col-6">
+                        <div class="card text-center bg-light">
+                            <img src="/site/img/homeBackground.png" class="card-img-top">
+                        </div>
+                    </div>
+                    <div class="col-6">
+                        <div class="d-flex flex-column ml-3 text-left">
+                            <span class="custom-span truncar-1l">TITULO DO ARTIGO</span>
+                            <span class="custom-span span-resumo">Resumo do artigo com no máximo 5 linhas de texto. Caso não haja um resumo, as 5 primeiras linhas do artigo serão exibidas aqui.</span>
+                        </div>
+                    </div>
+                </a>
+            </div>
+        </div>
+
+
+        <div class="col-xl-6 col-lg-6 col-md-6 col-6">
+            <div class="row g-3 justify-content-center">
+                <a href="/" class="btn mt-2 d-flex align-items-center">
+                    <div class="col-6">
+                        <div class="card text-center bg-light">
+                            <img src="/site/img/homeBackground.png" class="card-img-top">
+                        </div>
+                    </div>
+                    <div class="col-6">
+                        <div class="d-flex flex-column ml-3 text-left">
+                            <span class="custom-span truncar-1l">TITULO DO ARTIGO</span>
+                            <span class="custom-span span-resumo">Resumo do artigo com no máximo 5 linhas de texto. Caso não haja um resumo, as 5 primeiras linhas do artigo serão exibidas aqui.</span>
+                        </div>
+                    </div>
+                </a>
+            </div>
+        </div>
+    </div>
+</div>
+
 @endsection
