d9463d57eabf2e510ac3a980d3e2f65975d72f14
Jean Carlo Corso <jeancarlocorso@hotmail.com>
2024-02-28 12:25:52 -0300

    tela de posts da area restrita finalizada

--- BlogPsicologia/resources/views/AreaRestrita/homeRestrito.blade.php
+++ BlogPsicologia/resources/views/AreaRestrita/homeRestrito.blade.php
@@ -3,6 +3,7 @@
 @section('css')
     <link rel="stylesheet" href={{ asset('css/botao.css') }}>
     <link rel="stylesheet" href={{ asset('css/homeAr.css') }}>
+    <link rel="stylesheet" href={{ asset('css/tabelas.css') }}>
 @endsection
 
 @section('content')
@@ -23,4 +24,65 @@
     </div>
 </div>
 
+<br/>
+
+@php
+    $posts = [
+        (object) [
+            'id' => 1,
+            'titulo' => 'Teste de post',
+            'data' => '08/02/2024 08:05:00 PM',
+            'url'  => route('login'), 
+        ],
+        (object) [
+            'id' => 2,
+            'titulo' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+            'data' => '09/10/2023 12:30:00 AM',
+            'url'  => route('login'),
+        ],
+    ];
+@endphp
+
+<div class="row">
+    <table>
+        <thead>
+            <tr>
+                <th>Título</th>
+                <th>Data</th>
+                <th>Gerenciar</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($posts as $post)
+                <tr class="clickable-row" onclick="window.location='{{ $post->url }}';">
+                    <td class="left">{{ $post->titulo }}</td>
+                    <td>{{ $post->data }}</td>
+                    <td>
+                        <div class="dropdown">
+                            <button class="btn btn-secondary dropdown-trigger" type="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                <i class="fas fa-ellipsis-v"></i>
+                            </button>
+                            <div class="dropdown-menu dropdown-menu-end" aria-labelledby="dropdownMenuButton">
+                                <a class="dropdown-item" href="#"><i class="fas fa-pencil-alt"></i> Editar</a>
+                                <a class="dropdown-item text-danger" href="#"><i class="fas fa-trash-alt"></i> Apagar</a>
+                            </div>
+                        </div>
+                    </td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
+</div>
+
+<script>
+    document.addEventListener("DOMContentLoaded", function() {
+        var dropdownTriggers = document.querySelectorAll('.dropdown-trigger');
+
+        dropdownTriggers.forEach(function(dropdownTrigger) {
+            dropdownTrigger.addEventListener('click', function(event) {
+                event.stopPropagation();
+            });
+        });
+    });
+</script>
 @endsection
